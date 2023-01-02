8829e0feedc935cc6ad1abab9f43937eb853b367
Miguel Antunes <miguel.antunes.costa10@gmail.com>
2023-01-02 14:25:06 -0300

    Primeira Classe: Posicao

--- src/application/App.java
+++ src/application/App.java
@@ -1,11 +1,15 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello Word!");
+		Posicao pos = new Posicao(3, 5);
+		
+		System.out.println(pos);
 	}
 
 }
