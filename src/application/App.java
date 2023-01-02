35c770a1c1c056428161801683ad46ae8d89a0c9
Miguel Antunes <miguel.antunes.costa10@gmail.com>
2023-01-02 14:49:37 -0300

    começando a implementar tabuleiro e peça

--- src/application/App.java
+++ src/application/App.java
@@ -1,15 +1,14 @@
 package application;
 
 import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
 
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		Posicao pos = new Posicao(3, 5);
+		Tabuleiro tabuleiro = new Tabuleiro(8,8);
 		
-		System.out.println(pos);
 	}
 
 }
